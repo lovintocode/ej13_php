6d5fc7fefb06eeaaff32f74213bf4b7b4862df83
lovintocode <marcviagil@gmail.com>
2021-01-14 14:34:00 +0100

    Segundo commit

---
+++ conexion.php
@@ -0,0 +1,21 @@
+<?php
+	class Conexion{
+		private $conexion;
+		function __construct(){
+			$json_data = file_get_contents("config.json");
+			$array = json_decode($json_data, true);
+
+			try{
+				$this->conexion = new PDO("mysql:host=".$array['ruta'].";dbname=".$array['dbname']."", "".$array['usr']."","".$array['pwd']."");
+			}catch(Exception $e){
+				die('Error : '.$e->GetMessage());
+			}
+		}
+		function getConector(){
+			return $this->conexion;
+		}
+		function cerrar(){
+			$conexion = null;
+		}
+	}
+?>
