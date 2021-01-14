2a41d87b822fd4045d8fca6bd07dc62d1aeb3d47
lovintocode <marcviagil@gmail.com>
2021-01-14 18:14:28 +0100

    Add html folder and first part of the exercice

--- conexion.php
+++ conexion.php
@@ -1,19 +1,23 @@
 <?php
 	class Conexion{
+
 		private $conexion;
+
 		function __construct(){
 			$json_data = file_get_contents("config.json");
 			$array = json_decode($json_data, true);
-
 			try{
 				$this->conexion = new PDO("mysql:host=".$array['ruta'].";dbname=".$array['dbname']."", "".$array['usr']."","".$array['pwd']."");
+				$this->conexion->exec("set character set utf8");				
 			}catch(Exception $e){
 				die('Error : '.$e->GetMessage());
 			}
 		}
+
 		function getConector(){
 			return $this->conexion;
 		}
+		
 		function cerrar(){
 			$conexion = null;
 		}
--- index.php
+++ index.php
@@ -6,38 +6,73 @@
     <title>Document</title>
 </head>
 <body>
-    
     <?php
-        require "bbdd.php";
+
+        require "conexion.php";
         include "registro.html";
         
-
+        // Se ejecuta al crear un usuario
         if(isset($_POST['crear'])){
             $nombre = $_POST['usuario'];
-            $pass = $_POST['pass'];
-            $tipo = $_POST['tipo'];
-            $crypted_pass = encriptar($pass);
-            
-
-        }
-        function encriptar($pass){
-            return password_hash($pass, PASSWORD_DEFAULT);
-        }
-        function checkExists($nombre){
-            $conexion = 
-            $consulta = "SELECT cnombre FROM usuarios WHERE cnombre=:nombre";
-            $stmt = $conexion -> prepare($consulta);
-            $stmt->bindParam(":nombre", $nombre);
-            if($stmt->execute()){
-
-                if($stmt->fetch()){
-                    print_r("Existe");
-                }
-                while($fila = $stmt->fetch()){
-                    print_r("Existe");
-                }
-                $stmt->exit();
+            $pass = encriptar();
+            $tipo = extractTipo();
+            if(!checkExists("cnombre", $nombre, "usuarios")){
+                insertarUsuario($nombre, $pass, $tipo);
             }
+        }
+
+        // Encripta una contraseña enviada por "POST" y name "pass"
+        function encriptar(){
+            $pass = $_POST['pass'];
+            return password_hash($pass, CRYPT_BLOWFISH, array("cost"=>10));
+        }
+
+        // Inserta un usuario en la tabla "usuarios" (nombre, pass, tipo)
+        function insertarUsuario($nombre, $pass, $tipo){
+            $conexion_ini = new Conexion();
+            $conexion = $conexion_ini->getConector();
+            $consulta = "INSERT INTO usuarios VALUES (NULL, :nombre, :pwd, :tipo)";
+            $stmt = $conexion->prepare($consulta, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $stmt->bindParam(":nombre", $nombre);
+            $stmt->bindParam(":pwd", $pass);
+            $stmt->bindParam(":tipo", $tipo);
+            $stmt->execute();
+            $stmt->closeCursor();
+            $conexion_ini->cerrar();
+            echo "Usuario insertado";
+        }
+
+        // Comprueba si existe un usuario mediante un codigo en la tabla especificada
+        function checkExists($campo, $valor, $tabla){
+            $exists = false;
+            $conexion_ini = new Conexion();
+            $conexion = $conexion_ini->getConector();
+            $consulta = "SELECT $campo FROM $tabla WHERE cnombre=:nombre";
+            $stmt = $conexion -> prepare($consulta, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $stmt->bindParam(":nombre", $valor);
+            if($stmt->execute()){
+                if($stmt->fetch(PDO::FETCH_ASSOC)){
+                    $exists = true;
+                }
+                $stmt->closeCursor();
+            }
+            $conexion_ini->cerrar();
+            return $exists;
+        }
+
+        // Comprueba que los datos introducidos son correctos
+        function checkData($nombre, $pass){
+            if($nombre != '' && $pass != ''){
+                return true;
+            }
+            return false;
+        }
+        
+        // Cambia el nombre del tipo a un int
+        function extractTipo(){
+            $tipo = $_POST['tipo'];
+            if($tipo == 'admin') return 1;
+            else return 0;
         }
     ?>
 </body>
